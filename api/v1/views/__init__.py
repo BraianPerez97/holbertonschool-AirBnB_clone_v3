311afbcf018e6d54a1963eaf06380c19e465a1da
Jonathan Perez <jonathanperez604@gmail.com>
2023-08-25 17:17:21 -0400

    Delete api directory

--- api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-# Create a Blueprint instance for the views
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-# Import the views
-from api.v1.views.index import *
--- api/v1/views/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Index Script"""
-from api.v1.views import app_views
-from flask import jsonify
-
-# Create a route for /status
-@app_views.route('/status', methods=['GET'])
-def status():
-    return jsonify(status="OK")
