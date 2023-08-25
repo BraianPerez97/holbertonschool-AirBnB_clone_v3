6c9f0b6ce0ad60bf362d7dcb666923637355f095
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-25 19:20:52 -0400

    test app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,44 +3,45 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-import models
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-"""flask instance"""
+
+# Get host and port from environment variables, or use default values
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+
+# Disable strict slashes in URL routing
 app.url_map.strict_slashes = False
+
+# Register the blueprint for API views
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
 
+# Enable CORS for the entire application, allowing requests from the specified host
+CORS(app, resources={r"/*": {"origins": app_host}})
 
+# Define a function to close the database connection after each request
 @app.teardown_appcontext
 def teardown_flask(exception):
     """Flask app/request event listener."""
-    # Prints exception
     storage.close()
 
-
+# Define a custom error handler for 404 (Not Found) errors
 @app.errorhandler(404)
 def error_404(error):
-    """handles 404 error code"""
+    """Handles 404 error code"""
     return jsonify(error='Not found'), 404
 
-
+# Define a custom error handler for 400 (Bad Request) errors
 @app.errorhandler(400)
 def error_400(error):
-    """handles 400 error code"""
+    """Handles 400 error code"""
     msg = 'bad request'
     if isinstance(error, Exception) and hasattr(error, 'description'):
         msg = error.description
     return jsonify(error=msg), 400
 
-
+# Run the application only when this script is executed directly
 if __name__ == '__main__':
-    app_host = os.getenv("HBNB_API_HOST", '0.0.0.0')
-    app_port = int(os.getenv("HBNB_API_PORT", '5000'))
-    app.run(host=app_host,
-            port=app_port,
-            threaded=True)
+    app.run(host=app_host, port=app_port, threaded=True)
