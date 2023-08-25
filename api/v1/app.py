95bed479647855678e2ad1eb039c39c01803edec
BraianPerez97 <3603@holbertonstudents.com>
2023-08-25 14:53:18 -0400

    adding index and app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 """Server file"""
-from flask import Flask
+import os
+from flask import Flask, jsonify
+
 import models
+from models import storage
 from api.v1.views import app_views
 
-app = Flask(__name__)
 
-app.register_blueprint(app_views)
+def create_app(config_name):
+    app = Flask(__name__)
+
+    app.config.from_object(config_name)
+
+    app.register_blueprint(app_views)
+
+    @app.teardown_appcontext
+    def close_db(error):
+        storage.close()
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"error": "Not found"}), 404
+
+    return app
 
 
-@app.teardown_appcontext
-def close_db(error):
-    storage.close()
-
+app = create_app(os.getenv('APP_CONFIG', 'default'))
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', 5000)),
-            threaded=True)
-
-app = Flask(__name__)
-
-app.register_blueprint(app_views)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
