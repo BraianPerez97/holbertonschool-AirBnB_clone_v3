c152543d9c98cb75b9f6e8a00076082f6433597c
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-25 19:21:17 -0400

    test index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,21 +4,32 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-
+# Define a route to check the status of the API
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
 
-
+# Define a route to get statistics about the objects
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
-    """returns the number of each objects by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+    """ Returns JSON with object counts """
+    # Get the counts using the storage.count() method
+    amenities_count = storage.count('Amenity')
+    cities_count = storage.count('City')
+    places_count = storage.count('Place')
+    reviews_count = storage.count('Review')
+    states_count = storage.count('State')
+    users_count = storage.count('User')
+
+    # Create a JSON response with the counts
+    counts = {
+        "amenities": amenities_count,
+        "cities": cities_count,
+        "places": places_count,
+        "reviews": reviews_count,
+        "states": states_count,
+        "users": users_count
+    }
+    
+    return jsonify(counts)
