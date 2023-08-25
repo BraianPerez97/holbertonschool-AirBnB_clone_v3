587373325d758a76ecb4b073749fa2a5fa84a7d8
BraianPerez97 <3603@holbertonstudents.com>
2023-08-25 14:19:49 -0400

    adding api

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Server file"""
+from flask import Flask
+import models
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', 5000)),
+            threaded=True)
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
