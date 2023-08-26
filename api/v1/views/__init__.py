4be99999769f3b344e17e2e5d64b3493258dcde4
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-26 16:34:52 -0400

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,12 @@
 """Contains the API blueprint"""
 from flask import Blueprint
 
-# Create the blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import views to be included in the blueprint
-from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places import *
 from api.v1.views.states import *
+from api.v1.views.index import *
